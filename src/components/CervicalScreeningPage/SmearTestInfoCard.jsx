cd15250e739e1410e50258089d30f8b31ba2d4b1
DirushanDev <rubakumardirushan@gmail.com>
2025-09-19 10:07:55 +0530

    added

--- src/components/CervicalScreeningPage/SmearTestInfoCard.jsx
+++ src/components/CervicalScreeningPage/SmearTestInfoCard.jsx
@@ -32,7 +32,7 @@ export default function SmearTestInfoCard() {
               <path d="M22 21v-2a4 4 0 0 0-3-3.87" />
               <path d="M16 3.13a4 4 0 0 1 0 7.75" />
             </svg>
-            🧍‍♀️ Who Needs a Smear Test?
+             Who Needs a Smear Test?
           </div>
         </div>
 
