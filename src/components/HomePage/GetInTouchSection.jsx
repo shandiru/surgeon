85dfa4609a9ee713192b1deaed32fa7cab72bfa3
Sanjshine99 <sanubandara12639@gmail.com>
2026-02-24 14:46:09 +0530

    fix(): contact detials and content change

--- src/components/HomePage/GetInTouchSection.jsx
+++ src/components/HomePage/GetInTouchSection.jsx
@@ -26,7 +26,7 @@ export default function GetInTouchSection() {
       title: "Park Hospital",
       subtitle: "Private Consultations",
       btn: "Book Private Appointment",
-      link: "https://www.circlehealthgroup.co.uk/online-booking?consultant=C6083162",
+      link: "https://www.circlehealthgroup.co.uk/consultants/ketankumar-gajjar",
       icon: (
         <>
           <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z" />
@@ -38,6 +38,7 @@ export default function GetInTouchSection() {
       title: "Spire Nottingham Hospital",
       subtitle: "Private Practice",
       btn: "Schedule Consultation",
+      link: "https://www.spirehealthcare.com/consultant-profiles/mr-ketan-gajjar-c6083162/",
       icon: (
         <>
           <path d="M8 2v4" />
